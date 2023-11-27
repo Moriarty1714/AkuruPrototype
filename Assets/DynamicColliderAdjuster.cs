5c51c442f6ea21a6ded9a2f68ee996603bba2728
Marti De Ferrer Fuertes <marti.deferrer@enti.cat>
2023-11-27 16:14:05 +0100

    Tutorial Filter

--- Assets/DynamicColliderAdjuster.cs
+++ Assets/DynamicColliderAdjuster.cs
@@ -10,11 +10,10 @@ public class DynamicColliderAdjuster : MonoBehaviour
     public BoxCollider2D downRightCollider;
 
     private Transform tutorialBlackFilter;
+    private Vector2 centerFilter;
 
     void Start()
     {
-        tutorialBlackFilter = GetComponent<Transform>();
-
         FitToScreen(tutorialBlackFilter, camera);
 
         //AdjustColliders();
@@ -22,32 +21,8 @@ public class DynamicColliderAdjuster : MonoBehaviour
 
     void Update()
     {
-        AdjustColliders();
+
     }
-
-    void AdjustColliders()
-    {
-        // Tamaño de la pantalla en unidades del mundo
-        Vector2 screenSize = new Vector2(camera.orthographicSize * camera.aspect * 2, camera.orthographicSize * 2);
-
-        // Posición y escala del cuadrado blanco
-        Vector2 holePosition = (Vector2)smallSquare.position;
-        Vector2 holeSize = smallSquare.localScale;
-
-        // Calcular el tamaño del collider 'upLeft'
-        Vector2 upLeftSize = new Vector2(Mathf.Abs((screenSize.x / 2) - (holePosition.x - holeSize.x / 2)), Mathf.Abs((screenSize.y / 2) - (holePosition.y + holeSize.y / 2)));
-
-        // Calcular el desplazamiento del collider 'upLeft'
-        Vector2 upLeftOffset = new Vector2((-screenSize.x / 2) + (upLeftSize.x / 2), (screenSize.y / 2) - (upLeftSize.y / 2));
-
-        // Aplicar tamaño y desplazamiento al collider 'upLeft'
-        upLeftCollider.size = upLeftSize;
-        upLeftCollider.offset = upLeftOffset + new Vector2((holeSize.x / 2), -(holeSize.y / 2));
-    }
-
-
-
-
 
     public static void FitToScreen(Transform transform, Camera camera)
     {
@@ -65,3 +40,5 @@ public class DynamicColliderAdjuster : MonoBehaviour
         }
     }
 }
+
+
