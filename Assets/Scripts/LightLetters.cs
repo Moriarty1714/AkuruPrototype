6ec43a10c260aeceafb390f7c61691fc900cecf1
ArnauMerin99 <114000254+ArnauMerin99@users.noreply.github.com>
2023-12-04 18:01:20 +0100

    funcion iluminar

--- Assets/Scripts/LightLetters.cs
+++ Assets/Scripts/LightLetters.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using UnityEngine;
+using DG.Tweening;
+using System;
 
 public class LightLetters : MonoBehaviour
 {
     public float duracionIluminacion = 0.5f; // Duración en segundos de la iluminación
     public GameObject[] lettersAIluminar; // Array de GameObjects a iluminar en secuencia
     public GameObject[] objetosParaIluminar; // Array de GameObjects a iluminar en secuencia
+    public GameObject objetoAMover; // Array de GameObjects a iluminar en secuencia
 
     private SpriteRenderer[] spriteRenderers;
     private LetterController[] letterControllers;
-    private Coroutine secuenciaCoroutine;
+    private Transform[] transformLetters;
+
+
+
+    private Coroutine secuenciaCoroutine, sequenciaReq;
 
     void Start()
     {
+        DOTween.Init(true, true, LogBehaviour.Verbose).SetCapacity(200, 10);
+
+
         // Obtener los SpriteRenderers de los GameObjects
+
         spriteRenderers = new SpriteRenderer[objetosParaIluminar.Length];
-
-
         for (int i = 0; i < objetosParaIluminar.Length; i++)
         {
             spriteRenderers[i] = objetosParaIluminar[i].GetComponent<SpriteRenderer>();
@@ -27,15 +36,31 @@ public class LightLetters : MonoBehaviour
         {
             letterControllers[i] = lettersAIluminar[i].GetComponent<LetterController>();
         }
+        
+        transformLetters = new Transform[lettersAIluminar.Length];
+        for (int i = 0; i < lettersAIluminar.Length; i++)
+        {
+            transformLetters[i] = lettersAIluminar[i].GetComponent<Transform>();
+        }
+
+        //Set la pos del objeto que se mueve a la primera letra introducida
+        objetoAMover.transform.position = transformLetters[0].position;
 
         // Iniciar la secuencia de iluminación
         IniciarSecuenciaIluminacion();
-    }
+
+        IniciarSecuenciaRecorrido();
+    }   
 
     void IniciarSecuenciaIluminacion()
     {
         secuenciaCoroutine = StartCoroutine(IluminarSecuencia());
     }
+    void IniciarSecuenciaRecorrido()
+    {
+        RecorridoSequencia();
+    }
+
 
     void DetenerSecuenciaIluminacion()
     {
@@ -44,10 +69,19 @@ public class LightLetters : MonoBehaviour
             StopCoroutine(secuenciaCoroutine);
         }
     }
+    
+    void DetenerSecuenciaRecorrido()
+    {
+        if (sequenciaReq != null)
+        {
+            StopCoroutine(sequenciaReq);
+
+            //sequenciaReq = StartCoroutine(RecorridoSeq());
+        }
+    }
 
     IEnumerator IluminarSecuencia()
     {
-
         for (int i = 0; i < lettersAIluminar.Length; i++)
         {
             if(letterControllers[i].letterState == LetterState.NORMAL)
@@ -62,12 +96,44 @@ public class LightLetters : MonoBehaviour
                 spriteRenderers[i].color = new Color(1f, 1f, 1f, 0f); // Puedes ajustar el color según tus necesidades
 
                 // Esperar un breve tiempo antes de pasar al siguiente objeto
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(duracionIluminacion);
 
             }
         }
-
         // Reiniciar la secuencia 
         IniciarSecuenciaIluminacion();
     }
+
+
+    void RecorridoSequencia()
+    {
+        DOTween.Clear();
+        objetoAMover.transform.position = letterControllers[0].transform.position;
+
+        for (int i = 0; i < lettersAIluminar.Length; i++)
+        {
+            if (letterControllers[i].letterState == LetterState.NORMAL)
+            {
+                objetoAMover.transform.DOMove(transformLetters[i].position, duracionIluminacion*2f).SetEase(Ease.InOutSine).OnKill(RecorridoSequencia);
+            }
+        }
+
+        //objetoAMover.SetActive(false);
+    }
+
+    //IEnumerator RecorridoSeq()
+    //{
+    //    objetoAMover.transform.position = letterControllers[0].transform.position;
+
+    //    for (int i = 0; i < lettersAIluminar.Length; i++)
+    //    {
+    //        if (letterControllers[i].letterState == LetterState.NORMAL)
+    //        {
+    //            objetoAMover.transform.DOMove(transformLetters[i].position, duracionIluminacion).SetEase(Ease.InOutSine).OnComplete(DetenerSecuenciaRecorrido);
+    //        }
+    //    }
+
+    //    yield return new WaitForSeconds(4f);
+        
+    //}
 }
