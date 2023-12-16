106605f5990818644ba550e806d054195121cec0
ArnauMerin99 <114000254+ArnauMerin99@users.noreply.github.com>
2023-12-16 20:20:48 +0100

    Un poco de particulas :D

--- Assets/Scripts/MouseEffects.cs
+++ Assets/Scripts/MouseEffects.cs
@@ -6,24 +6,8 @@ public class MouseEffects : MonoBehaviour
 {
     [SerializeField] private GameObject particle;
 
-    [Header("Audio Settings")]
-    [SerializeField] private AudioClip clickSound;
-    [Range(0.1f, 0.5f)]
-    public float volumeChangeMultiplayer = 0.2f;
-    [Range(0.1f, 0.5f)]
-    public float pichChangeMultiplayer = 0.2f;
-
-
     private Vector2 mousePos;
-    private AudioSource audioSource;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -33,8 +17,6 @@ public class MouseEffects : MonoBehaviour
 
 
             AudioManager.Instance.PlaySFX("TouchScreen");
-
-            audioSource.Play();                                                                     //Play audio
         }
     }
 }
